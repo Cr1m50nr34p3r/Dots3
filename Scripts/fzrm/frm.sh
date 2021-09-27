b41e4e63b52a85e5b4720681f81a3545041fec88
akshatgarg789 <akshatgarg789@gmail.com>
2021-09-27 21:34:02 +0530

    added dot-port.sh

--- Scripts/fzrm/frm.sh
+++ Scripts/fzrm/frm.sh
@@ -4,7 +4,7 @@ case $1 in
     'f' | 'F')
 
         file1=$( find . $HOME | fzf -m --preview=" cat {} || clear && ls {} " --prompt="Select File/Directory: " --bind="?:toggle-preview" | sed 's/^\./\/home\/r3dbl4d3/g' )
-        [[ -f $file1 ]] && rm -i $file1 || rm -rfi $file1
+        [[ -f $file1 ]] && rm -i $file1 || rm -rf $file1
         ;;
     'r' |'R')
         file1=$( find . $HOME | rofi -dmenu -normal-window | sed 's/^\./\/home\/r3dbl4d3/g' )
